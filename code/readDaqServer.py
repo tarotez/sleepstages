3cdf6e24f42f3fc6d30953b918ba9c869e3b94e9
tarotez <tez@sings.jp>
2021-05-09 14:27:07 +0900

    separate ch2

--- code/readDaqServer.py
+++ code/readDaqServer.py
@@ -73,10 +73,12 @@ class ReadDAQServer:
         - reserved
         """
         try:
-            DAQmxReadAnalogF64(taskHandle, self.numSampsPerChan, self.timeout,
-                         DAQmx_Val_GroupByChannel, data, self.numSampsPerChan * self.channelNum, byref(int32()), None)
+            # DAQmxReadAnalogF64(taskHandle, self.numSampsPerChan, self.timeout,
+            #             DAQmx_Val_GroupByChannel, data, self.numSampsPerChan * self.channelNum, byref(int32()), None)
             # DAQmxReadAnalogF64(taskHandle, 1, self.timeout,
             #             DAQmx_Val_GroupByChannel, data, self.channelNum, byref(int32()), None)
+            DAQmxReadAnalogF64(taskHandle, self.numSampsPerChan, self.timeout,
+                         DAQmx_Val_GroupByScanNumber, data, self.numSampsPerChan * self.channelNum, byref(int32()), None)
 
         except:
             import sys
