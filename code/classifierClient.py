fixed normalization procedure

code/classifierClient.py
@@ -9,6 +9,7 @@ import timeFormatting
 # from filters import butter_lowpass_filter
 from algorithmFactory import AlgorithmFactory
 from deepClassifier import DeepClassifier
+from statistics import standardize, standardizer
 # from ksstatistics import StatisticalTester
 # from fileManagement import readStandardMice, readdMat, readdTensor
 
@@ -147,6 +148,10 @@ class ClassifierClient:
         self.predFileBeforeOverwrite = open(self.params.predDir + '/' + self.predFileID + '_pred_before_overwrite.txt', 'w')
         self.predFileWithTimeStamps = open(self.params.predDir + '/' + self.predFileID + '_pred_with_timestamps.txt', 'w')
 
+        self.max_storage_for_standardization = self.samplePointNum * 1000
+        self.standardizer_eeg = standardizer(self.max_storage_for_standardization)
+        self.standardizer_ch2 = standardizer(self.max_storage_for_standardization)
+
     def setStagePredictor(self, classifierID):
         paramFileName = 'params.' + str(classifierID) + '.json'
         finalClassifierDir = self.params.finalClassifierDir
@@ -221,8 +226,16 @@ class ClassifierClient:
             self.windowStartTime = timeStampSegment[0]
 
         # print('eegFragment =', eegFragment)
+        ### orig_one_record_partial, orig_raw_one_record_partial = self.normalize_eeg(eegFragment, ch2Fragment, self.past_eegSegment, self.past_ch2Segment)
+        standardized_eegFragment = self.standardizer_eeg.standardize(eegFragment)
+        if self.ch2_normalize_for_prediction:
+            standardized_ch2Fragment = self.standardizer_ch2.standardize(ch2Fragment)
+        else:
+            standardized_ch2Fragment = ch2Fragment
+        one_record_partial = np.array((standardized_eegFragment, standardized_ch2Fragment)).transpose()
+        raw_one_record_partial = np.array((eegFragment, ch2Fragment)).transpose()
 
-        one_record_partial, raw_one_record_partial = self.normalize_eeg(eegFragment, ch2Fragment, self.past_eegSegment, self.past_ch2Segment)
+
         self.one_record[self.sampleID:(self.sampleID+self.updateGraph_samplePointNum),:] = one_record_partial
         self.raw_one_record[self.sampleID:(self.sampleID+self.updateGraph_samplePointNum),:] = raw_one_record_partial
         one_record_for_graph_partial = self.normalize_one_record_partial_for_graph(raw_one_record_partial, self.past_eegSegment, self.past_ch2Segment)

code/networkServer.py
@@ -65,6 +65,8 @@ class NetworkServer:
             received_data = tcp_client.recv(BUFSIZE)
             observed_samplingFreq = struct.unpack_from('H', received_data, 0)[0]    #WORD
             observed_epochTime = struct.unpack_from('H', received_data, 2)[0]    #WORD
+            print('observed_samplingFreq =', observed_samplingFreq)
+            print('observed_epochTime =', observed_epochTime)
             observed_samplePointNum = observed_samplingFreq * observed_epochTime
             classifierID, model_samplingFreq, model_epochTime = selectClassifierID(self.params_for_classifier.finalClassifierDir, networkName, observed_samplingFreq, observed_epochTime)
             model_samplePointNum = model_samplingFreq * model_epochTime
@@ -83,7 +85,7 @@ class NetworkServer:
                     # try:
                         received_data = tcp_client.recv(BUFSIZE)
                         # print('received_data =', received_data)
-                        # print('len(received_data) =', len(received_data))
+                        #print('len(received_data) =', len(received_data))
 
                         if len(received_data) == 0:
                             exit()
@@ -125,11 +127,12 @@ class NetworkServer:
 
                             # EEG data
                             signalW = struct.unpack_from(fmt, received_data, 22)    #float
+                            #print('len(signalW) =', len(signalW))
                             signal_rawarray = np.array(signalW, dtype='float64')
-                            # print('before up/down sampling, signal_rawarray.shape =', signal_rawarray.shape)
+                            #print('before up/down sampling, signal_rawarray.shape =', signal_rawarray.shape)
 
-                            # print('model_samplePointNum =', model_samplePointNum)
-                            # print('observed_samplePointNum =', observed_samplePointNum)
+                            #print('model_samplePointNum =', model_samplePointNum)
+                            #print('observed_samplePointNum =', observed_samplePointNum)
 
                             # downsampling
                             if model_samplePointNum < observed_samplePointNum:

code/sequentialPrediction.py
@@ -3,7 +3,7 @@ import numpy as np
 from os.path import dirname, abspath
 from parameterSetup import ParameterSetup
 from stagePredictor import StagePredictor
-from statistics import standardize
+from statistics import standardizer
 from algorithmFactory import AlgorithmFactory
 from deepClassifier import DeepClassifier
 
@@ -99,7 +99,9 @@ def classifySequentially(params, paramID, paramDir, fileIDpair):
     # while startSamplePoint + wsizeInSamplePointNum <= samplePointNum:
     records_L = []
     timeStampSegments_L = []
-    all_past_eeg, all_past_ch2 = np.array([]), np.array([])
+    ### all_past_eeg, all_past_ch2 = np.array([]), np.array([])
+    standardizer_eeg = standardizer(samplePointNum)
+    standardizer_ch2 = standardizer(samplePointNum)
     # standardized_all_past_eeg = np.array([])
     for startSamplePoint in range(0, samplePointNum, timeWindowStrideInSamplePointNum):
         endSamplePoint = startSamplePoint + wsizeInSamplePointNum
@@ -107,10 +109,13 @@ def classifySequentially(params, paramID, paramDir, fileIDpair):
             break
         timeStampSegment = timeStamps[startSamplePoint:endSamplePoint]
         eegSegment = eeg[startSamplePoint:endSamplePoint]
-        standardized_eegSegment, all_past_eeg = standardize(eegSegment, all_past_eeg)
+        ### standardized_eegSegment, all_past_eeg = standardize(eegSegment, all_past_eeg)
+        standardized_eegSegment = standardizer_eeg.standardize(eegSegment)
+
         if params.useEMG:
             ch2Segment = ch2[startSamplePoint:endSamplePoint]
-            standardized_ch2Segment, all_past_ch2 = standardize(ch2Segment, all_past_ch2)
+            ### standardized_ch2Segment, all_past_ch2 = standardize(ch2Segment, all_past_ch2)
+            standardized_ch2Segment = standardizer_ch2.standardize(ch2Segment)
             one_record = np.r_[standardized_eegSegment, standardized_ch2Segment]
         else:
             one_record = standardized_eegSegment