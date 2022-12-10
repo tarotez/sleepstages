b0dc76afcde19514708e82449465f909c26c7c28
tarotez <tez@sings.jp>
2022-12-10 22:35:42 +0200

    bugfix: checkGPU.py by mitometa

--- code/checkGPU.py
+++ code/checkGPU.py
@@ -1,11 +1,11 @@
 import tensorflow as tf
 
-# Creates a graph.
 with tf.device('/device:GPU:0'):
     a = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[2, 3], name='a')
     b = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[3, 2], name='b')
     c = tf.matmul(a, b)
-# Creates a session with log_device_placement set to True.
-sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
-# Runs the op.
-print(sess.run(c))
+
+tf.compat.v1.disable_eager_execution()
+with tf.compat.v1.Session(config=tf.compat.v1.ConfigProto(log_device_placement=True)) as sess:
+    tem = tf.constant(c)
+    print(sess.run(tem))
